6cd219d1e5268eef04b2ef36545e13ddecc89e76
jeongyoonlee <jeongyoonlee@users.noreply.github.com>
2020-12-28 20:13:40 +0000

    deploy: 33b3bfdf6199bd9be6d31183c95e641f01866483

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/fastpages-20201228/assets/js/search-data.json', true);
+    request.open('GET', '/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
